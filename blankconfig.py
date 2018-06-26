0b4fe1719bcf102af937b54be83cb7259d11f0c0
TimRCM <tim@timrcm.com>
2018-06-26 12:59:20 -0600

    Tinkering with the table export

--- blankconfig.py
+++ blankconfig.py
@@ -1,10 +1,14 @@
+# Rename to config.py 
+
 server = ''
 database = ''
 
 username = ''
 password = ''
 
-table = '''SELECT 
+query = '''SELECT 
       [Col1] as 'Col1'
 	  ,[Col2] as 'Col2'
   FROM [DB].[dbo].[TABLE]'''
+
+columns = ['Col0', 'Col1', 'Col2', 'Col3', 'Col4', 'Col5', 'Col6']
--- main.py
+++ main.py
@@ -6,9 +6,33 @@ from flask_table import Table, Col
 
 import config
 
+
+class ItemTable(Table):
+    Col0 = Col(config.columns[0])
+    Col1 = Col(config.columns[1])
+    Col2 = Col(config.columns[2])
+    Col3 = Col(config.columns[3])
+    Col4 = Col(config.columns[4])
+    Col5 = Col(config.columns[5])
+    Col6 = Col(config.columns[6])
+
+class Item(object):
+    def __init__(self, Col0, Col1, Col2, Col3, Col4, Col5, Col6):
+        self.Col0 = Col0
+        self.Col1 = Col1
+        self.Col2 = Col2
+        self.Col3 = Col3
+        self.Col4 = Col4
+        self.Col5 = Col5
+        self.Col6 = Col6
+
+
 conn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER='+config.server+';DATABASE='+config.database+';UID='+config.username+';PWD='+config.password)
 export = conn.cursor()
 export.execute(config.query)
 
-for row in export:
-    print(f'{row}')
+# items = [Item(export[0], export[1], export[2], export[3], export[4], export[5], export[6])]
+items = [Item('item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7')]
+
+table = ItemTable(items)
+print(table.__html__())
