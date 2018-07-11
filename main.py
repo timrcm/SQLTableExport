94696cbe191b4e7504bdf6b8e3e52ae9383f55cc
TimRCM <tim@timrcm.com>
2018-07-11 13:52:37 -0600

    Framework for multi-OS support

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
 # Generates an HTML table from a SQL table using pandas 
 # 6.26.2018 TimRCM
 
+import os
 import pyodbc
-from pandas import DataFrame
+import pandas
 
 import config
 
-# Create a connection to the SQL database using pyodbc
-try:
-    conn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER='+config.server+';DATABASE='+config.database+';UID='+config.username+';PWD='+config.password)
-except:
-    print("Unknown connection error.")
+
+def connect_windows():
+    '''Create a connection to the SQL database using pyodbc & the SQL Server Native Client 11.0 driver in Windows 10,
+    then executes the SQL query to establish a Pandas DataFrame'''
+
+    try:
+        conn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER='+config.server+';DATABASE='+config.database+';UID='+config.username+';PWD='+config.password)
+    except:
+        print("Unknown connection error.")
+        exit(1)
+
+    export = conn.cursor()
+    # Execute the query in config.py 
+    export.execute(config.query)
+    # Establish a pandas DataFrame using from_records, which accepts 
+    # the list of tuples that will be contained in export
+    df = pandas.DataFrame.from_records(export)
+
+    return df
+
+
+def connect_unix():
+    df = print("Not yet functional on *nix/MacOS.")
     exit(1)
+    return df
 
-export = conn.cursor()
-# Execute the query in config.py 
-export.execute(config.query)
 
-# Establish a pandas DataFrame using from_records, which accepts 
-# the list of tuples that will be contained in export
-df = DataFrame.from_records(export)
-df.columns = config.columns
+def export_html(df):
+    df.columns = config.columns
+    # Dump the data to an HTML table 
+    df.to_html("export.html", border=1, justify='left', index=False)
 
-# Dump the data to an HTML table 
-html = df.to_html("export.html", border=1, justify='left', index=False)
+
+if __name__ == '__main__':
+    if os.name == 'nt':
+        df = connect_windows()
+        export_html(df)
+    else:
+        df = connect_unix()
+        export_html(df)
